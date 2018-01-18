a6f54592c1f302780ac34b5fd693d48d052ab004
avebryhv <33032139+avebryhv@users.noreply.github.com>
2018-01-18 23:16:15 +0000

    Re-organising folders

--- Classwork/week 5/Javascript_Advanced_Examples/Exercise2/main.js
+++
@@ -1,7 +0,0 @@
-
-function StorePlayerData()
-{
-	localStorage.setItem("playername",document.forms[0]["playername"].value);
-	localStorage.setItem("playerhealth",document.forms[0]["playerhealth"].value);
-	localStorage.setItem("playercolour",document.forms[0]["playercolour"].value);
-}
