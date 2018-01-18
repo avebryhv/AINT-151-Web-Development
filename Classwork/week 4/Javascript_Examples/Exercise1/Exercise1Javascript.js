138051b81416ce8e0eb24d9d36dfd29816e66ecf
avebryhv <33032139+avebryhv@users.noreply.github.com>
2018-01-18 23:17:52 +0000

    Re-organising folders

---
+++ Classwork/week 4/Javascript_Examples/Exercise1/Exercise1Javascript.js
@@ -0,0 +1,17 @@
+function AlertMessage()
+{
+    alert("Alert Message!");
+}
+
+
+function ConsoleMessage()
+{
+    console.log("Console is active");
+}
+
+var firstName = 'Jon';
+var lastName = 'Brown';
+function WhatsMyName()
+{
+    console.log(firstName+' '+lastName);
+}
